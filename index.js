4f8f688b8833502b3839f1563f725cac6e27595f
mark teng <mark_19475@hotmail.com>
2016-05-26 15:52:33 +0700

    Modify index respond, Add assets

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 var express = require("express");
+var logger = require("morgan");
+var bodyParser = require('body-parser');
 var app = express();
-var PORT = 8080;
 
-app.get('/',function(req,res){
-	res.send("Hello World");
+// create application/json parser 
+app.use(bodyParser.json());
+ 
+// create application/x-www-form-urlencoded parser 
+app.use(bodyParser.urlencoded({ extended: false }));
+
+var PORT = 8080;
+app.use(logger('dev'));
+app.use('/assets',express.static(__dirname+'/public/assets'));
+var users = [];
+
+app.post('/api/user',function(req,res){
+	users.push(req.body);
+	res.send('success');
+});
+app.get('/api/users',function(req,res){
+	res.send(users);
+});
+
+app.use('/',function(req,res){
+	res.sendFile(__dirname+'/public/index.html');
 });
 
 app.listen(PORT,function(){
